a54279df1216beb52388ce2e2f5cbfed5d7c00cf
MYMAN515 <gvt010609@gmail.com>
2026-01-09 19:04:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    unoptimized: true, // يساعد في تحميل الصور المحلية بدون مشاكل
+  },
 }
 
 module.exports = nextConfig
