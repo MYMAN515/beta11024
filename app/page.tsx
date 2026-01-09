02263b095264889002cdbf27af2b195e9ae5fc76
MYMAN515 <dr.rakradi@gmail.com>
2026-01-09 18:18:39 +0800

    Disable SSR for home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import HomeScreen from './components/HomeScreen'
+import dynamic from 'next/dynamic'
+
+const HomeScreen = dynamic(() => import('./components/HomeScreen'), {
+  ssr: false
+})
 
 export default function Home() {
   return <HomeScreen />
